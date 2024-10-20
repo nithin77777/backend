8d353d06661907ee9d0189641bb40b07c806d285
nithin <158035569+nithinSai98@users.noreply.github.com>
2024-10-20 18:10:37 +0530

    Working Code View name : index No Models Included

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+# from rest_framework.response import 
+
+@api_view(['GET'])
+def index(req):
+    return JsonResponse("Success" ,safe=False, status=status.HTTP_200_OK)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5g+)lc1o*8ae9^_+eh@uqbmny40!11emjjovp83c%cony0kws4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
