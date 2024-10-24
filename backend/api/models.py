c3c6c6386917b032e296c06af4658c8bdd656c85
nithin <158035569+nithinSai98@users.noreply.github.com>
2024-10-24 14:42:32 +0530

    fixtry: views.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.hashers import make_password
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    name = models.CharField(null=True, blank=True, max_length=100)
+    user_id = models.CharField(unique=True, max_length=26)
+    password = models.CharField(null=False, blank=False, max_length=16)
+
+    def save(self,*args, **kwargs):
+        if not self.password.startswith('pbkd'):
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
+              
+        
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.core.exceptions import ValidationError
+from django.contrib.auth.hashers import make_password, check_password
 
 # Create your views here.
 
+from rest_framework.views import APIView
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated, AllowAny
 # from rest_framework.response import 
 
 @api_view(['GET'])
 def index(req):
     return JsonResponse("Success" ,safe=False, status=status.HTTP_200_OK)
+
